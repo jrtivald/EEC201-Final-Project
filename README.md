2fa916e8308d58a3dace824e5684b6cec17970f5
Jonathan <boby828@gmail.com>
2021-02-07 15:19:25 -0800

    Added the aduio files from the links provided in the prompt as well as some informaiton on the recordings.  Also added a blank MatLab file to get us started.

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # EEC201-Final-Project
 Repo for EEC-201's final project at UCD, Winter 2021.
 
+# Audio
+Store different audio files used ot test the algorithm.
+
 # Docs
 Store any documents necssary for developing the project Matlab.
+
+# MatLab
+Stores the project source files written in Matlab
 
 # Report
 The LaTex source for the final report of the project.
---
+++ matlab/speaker-recognition.m
@@ -0,0 +1,17 @@
+%
+%EEC-201, Winter Quarter 2021, Final Project
+%
+%Title: Speaker Recognition
+%
+%Description: This is main function of the final project for EEC-201.
+%             This program will store features in the recorded audio of 
+%             different speakers in order to recognize which speaker is
+%             talking on further recordings.
+%
+%Authors: Igor Sheremet and Jonathan Tivald
+%
+%Date: 2/7/2021
+
+clear all;
+close all;
+clc;
