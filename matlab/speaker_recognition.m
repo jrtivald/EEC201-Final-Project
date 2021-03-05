865b23ba7c4e6011c45508f503fbfd761c6a10e7
Igor Sheremet <isheremet@ucdavis.edu>
2021-03-05 15:55:35 -0800

    - Added mel-spaced filter bank to MFCC - Added cepstrum to MFCC - various format changes

--- matlab/speaker_recognition.m
+++ matlab/speaker_recognition.m
@@ -11,38 +11,41 @@
 %
 % Date: 2/7/2021
 
-clear all;
+clear;
 close all;
 clc;
 
-%Parameters
-train_rec_cnt = 11;
-test_rec_cnt = 8;
-max_len = 30000;
-channel = 1;            %Some audio files have stereo
-pre_a = 0.95;           %Pre-Emphasis coefficient
-frame_size = 20;
-frame_overlap = 8;
-N_fft = 512;
+% Parameters
+TRAIN_REC_CNT    = 11;
+TEST_REC_CNT     = 8;
+MAX_LEN          = 30000;
+CHANNEL          = 1;            %Some audio files have stereo
+PRE_A            = 0.95;         %Pre-Emphasis coefficient
+FRAME_SIZE_MS    = 30;
+FRAME_OVERLAP_MS = 8;
+N_FFT            = 512;
+NUM_MEL_BANKS    = 20;
 
-%Trim down the files for the "important part"
-trim_len = 13000;
-train_trim_offset = 2500;
-test_trim_offset = 2500;
+% Trim down the files for the "important part"
+% TODO: make the word envelope detection automatic
+TRIM_LEN          = 13000;
+TRAIN_TRIM_OFFSET = 2500;
+TEST_TRIM_OFFSET  = 2500;
 
-%Read in a training file
-train_file = zeros([train_rec_cnt,max_len]);
-train_fs = zeros([train_rec_cnt,1]);
+%% Read in a training file
 
-for i = 1:train_rec_cnt
-    file = strcat('./data/Training_Data/s',num2str(i),'.wav');
+train_signal = zeros([TRAIN_REC_CNT,MAX_LEN]);
+train_fs = zeros([TRAIN_REC_CNT,1]);
+
+for i = 1:TRAIN_REC_CNT
+    file = strcat('../data/Training_Data/s',num2str(i),'.wav');
     [tmp_wav, tmp_fs] = audioread(file);
     
     %check if stereo
     if size(tmp_wav,2) > 1
-        train_file(i,1:length(tmp_wav)) = tmp_wav(:,channel);
+        train_signal(i,1:length(tmp_wav)) = tmp_wav(:,CHANNEL);
     else
-        train_file(i,1:length(tmp_wav)) = tmp_wav;
+        train_signal(i,1:length(tmp_wav)) = tmp_wav;
     end
     
     %Save Fs
@@ -51,34 +54,56 @@ for i = 1:train_rec_cnt
     disp(strcat(file,' length: ',num2str(length(tmp_wav))))
 end
 
-%Trim down training data
-train_file(:,1:trim_len) = train_file(:,train_trim_offset:(train_trim_offset+trim_len-1));
-train_file(:,trim_len+1:end) = [];
+% Trim down training data
+% TODO: make the word envelope detection automatic
+train_signal(:,1:TRIM_LEN) = train_signal(:,TRAIN_TRIM_OFFSET:...
+    (TRAIN_TRIM_OFFSET+TRIM_LEN-1));
+train_signal(:,TRIM_LEN+1:end) = [];
 
-%Read in a testing file
-test_file = zeros([test_rec_cnt,max_len]);
-test_fs = zeros([test_rec_cnt,1]);
+% % plot spectrograms of training data to visualize
+% figure('Name','Training Data Spectrograms')
+% for i = 1:TRAIN_REC_CNT
+%     subplot(2,ceil(TRAIN_REC_CNT/2),i)
+%     spectrogram(train_signal(i,:),hamming(FRAME_SIZE_MS/1000*train_fs(i)),...
+%         FRAME_OVERLAP_MS/1000*train_fs(i),N_FFT,train_fs(i));
+%     title(strcat('s',num2str(i),'.wav'))
+% end
 
-for i = 1:test_rec_cnt
-    file = strcat('./data/Test_Data/s',num2str(i),'.wav');
-    [tmp_wav, tmp_fs] = audioread(file);
+%% Read in a testing file
+
+test_signal = zeros([TEST_REC_CNT,MAX_LEN]);
+test_fs = zeros([TEST_REC_CNT,1]);
+
+for i = 1:TEST_REC_CNT
+    file = strcat('../data/Test_Data/s',num2str(i),'.wav');
+    [tmp_wav, test_fs(i)] = audioread(file);
     
-    %check if stereo
+    % check if stereo
     if size(tmp_wav,2) > 1
-        test_file(i,1:length(tmp_wav)) = tmp_wav(:,channel);
+        test_signal(i,1:length(tmp_wav)) = tmp_wav(:,CHANNEL);
     else
-        test_file(i,1:length(tmp_wav)) = tmp_wav;
+        test_signal(i,1:length(tmp_wav)) = tmp_wav;
     end
-
-    %Save Fs
-    test_fs(i) = tmp_fs;
     
     disp(strcat(file,' length: ',num2str(length(tmp_wav))))
 end
 
-%Trim down test data
-test_file(:,1:trim_len) = test_file(:,test_trim_offset:(test_trim_offset+trim_len-1));
-test_file(:,trim_len+1:end) = [];
+% Trim down test data
+% TODO: make the word envelope detection automatic
+test_signal(:,1:TRIM_LEN) = test_signal(:,TEST_TRIM_OFFSET:...
+    (TEST_TRIM_OFFSET+TRIM_LEN-1));
+test_signal(:,TRIM_LEN+1:end) = [];
 
-%Calculate the Mel-Frequency Cepstrum Coefficients
-tmp = mfcc(train_file(1,:), train_fs(1), pre_a, frame_size, frame_overlap, N_fft);
+% % plot spectrograms of test data to visualize
+% figure('Name','Test Data Spectrograms')
+% for i = 1:TEST_REC_CNT
+%     subplot(2,ceil(TEST_REC_CNT/2),i)
+%     spectrogram(test_signal(i,:),hamming(FRAME_SIZE_MS/1000*train_fs(i)),...
+%         FRAME_OVERLAP_MS/1000*train_fs(i),N_FFT,train_fs(i));
+%     title(strcat('s',num2str(i),'.wav'))
+% end
+
+%% Calculate the Mel-Frequency Cepstrum Coefficients
+
+tmp = mfcc(train_signal(1,:), train_fs(1), PRE_A, FRAME_SIZE_MS, ...
+    FRAME_OVERLAP_MS, N_FFT,NUM_MEL_BANKS);
