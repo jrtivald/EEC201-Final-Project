b8f36c15002875caf6fcba591e063fc4878c273e
imsheremet <78658687+imsheremet@users.noreply.github.com>
2021-03-13 15:22:33 -0800

    Word Detection (#4)
    * Initial commit for word envelope detection
    
    * Modified word detection parameters a bit so that the click at the start of signal 11 does not throw it off.
    
    * Cleaned up the code a bit
    
    * Cleaned up the code a bit and commented some plots out to speed it up.

--- matlab/speaker_recognition.m
+++ matlab/speaker_recognition.m
@@ -21,10 +21,14 @@ TEST_REC_CNT     = 8;
 MAX_LEN          = 30000;
 CHANNEL          = 1;            % Some audio files have stereo
 
+% Word Detection Parameters
+WORD_DETECT_THRESH_DB = -62;
+WORD_LENGTH_MS        = 550;
+
 % MFCC parameters
 FRAME_SIZE_MS    = 20;
-FRAME_OVERLAP_MS = 8;
-FFT_NUM_POINTS   = 512;
+FRAME_OVERLAP_MS = 10;
+FFT_NUM_POINTS   = 1024;
 MEL_NUM_BANKS    = 40;
 CEPS_START_BANK  = 2;
 CEPS_NUM_BANKS   = 15;
@@ -39,12 +43,6 @@ CODEBOOK_MFCC = [1 3 7 8 9 11];
 % NOTE: MFCCs to plot in FIGS MUST be in CODEBOOK_MFCC
 CODEBOOK_FIGS = [[1 9 7];
                  [11 3 1]];
-
-% Trim down the files for the "important part"
-% TODO: make the word envelope detection automatic
-TRIM_LEN          = 13000;
-TRAIN_TRIM_OFFSET = 2500;
-TEST_TRIM_OFFSET  = 2500;
 
 %% Read in a training file
 
@@ -68,20 +66,14 @@ for i = 1:TRAIN_REC_CNT
     disp(strcat(file,' length: ',num2str(length(tmp_wav))))
 end
 
-% Trim down training data
-% TODO: make the word envelope detection automatic
-train_signal(:,1:TRIM_LEN) = train_signal(:,TRAIN_TRIM_OFFSET:...
-    (TRAIN_TRIM_OFFSET+TRIM_LEN-1));
-train_signal(:,TRIM_LEN+1:end) = [];
-
-% plot spectrograms of training data to visualize
-figure('Name','Training Data Spectrograms')
-for i = 1:TRAIN_REC_CNT
-    subplot(2,ceil(TRAIN_REC_CNT/2),i)
-    spectrogram(train_signal(i,:),hamming(FRAME_SIZE_MS/1000*train_fs(i)),...
-        FRAME_OVERLAP_MS/1000*train_fs(i),FFT_NUM_POINTS,train_fs(i));
-    title(strcat('s',num2str(i),'.wav'))
-end
+% % plot spectrograms of training data to visualize
+% figure('Name','Training Data Spectrograms')
+% for i = 1:TRAIN_REC_CNT
+%     subplot(2,ceil(TRAIN_REC_CNT/2),i)
+%     spectrogram(train_signal(i,:),hamming(FRAME_SIZE_MS/1000*train_fs(i)),...
+%         FRAME_OVERLAP_MS/1000*train_fs(i),FFT_NUM_POINTS,train_fs(i));
+%     title(strcat('s',num2str(i),'.wav'))
+% end
 
 %% Read in a testing file
 
@@ -102,43 +94,85 @@ for i = 1:TEST_REC_CNT
     disp(strcat(file,' length: ',num2str(length(tmp_wav))))
 end
 
-% Trim down test data
-% TODO: make the word envelope detection automatic
-test_signal(:,1:TRIM_LEN) = test_signal(:,TEST_TRIM_OFFSET:...
-    (TEST_TRIM_OFFSET+TRIM_LEN-1));
-test_signal(:,TRIM_LEN+1:end) = [];
-
 % % plot spectrograms of test data to visualize
 % figure('Name','Test Data Spectrograms')
 % for i = 1:TEST_REC_CNT
 %     subplot(2,ceil(TEST_REC_CNT/2),i)
 %     spectrogram(test_signal(i,:),hamming(FRAME_SIZE_MS/1000*train_fs(i)),...
-%         FRAME_OVERLAP_MS/1000*train_fs(i),N_FFT,train_fs(i));
+%         FRAME_OVERLAP_MS/1000*test_fs(i),FFT_NUM_POINTS,test_fs(i));
 %     title(strcat('s',num2str(i),'.wav'))
 % end
 
 %% run signals through pre-emphasis filter
 
-train_signal_preemph = zeros(TRAIN_REC_CNT,TRIM_LEN);
+train_signal_preemph = zeros(TRAIN_REC_CNT,MAX_LEN);
 for i = 1:TRAIN_REC_CNT
    train_signal_preemph(i,:) = pre_emph(train_signal(i,:)); 
 end
 
-% plot filtered signals
-figure('Name','Filtered Training Data Spectrograms')
+% % plot filtered signals
+% figure('Name','Pre-Emphasis Filtered Training Data Spectrograms')
+% for i = 1:TRAIN_REC_CNT
+%     subplot(2,ceil(TRAIN_REC_CNT/2),i)
+%     spectrogram(train_signal_preemph(i,:),hamming(FRAME_SIZE_MS/1000*train_fs(i)),...
+%         FRAME_OVERLAP_MS/1000*train_fs(i),FFT_NUM_POINTS,train_fs(i));
+%     title(strcat('s',num2str(i),'.wav'))
+% end
+
+%% normalize signal amplitudes to [-1:1] range (ie auto. gain to full scale)
+
+train_signal_normalized = zeros(TRAIN_REC_CNT,MAX_LEN);
 for i = 1:TRAIN_REC_CNT
-    subplot(2,ceil(TRAIN_REC_CNT/2),i)
-    spectrogram(train_signal_preemph(i,:),hamming(FRAME_SIZE_MS/1000*train_fs(i)),...
-        FRAME_OVERLAP_MS/1000*train_fs(i),FFT_NUM_POINTS,train_fs(i));
-    title(strcat('s',num2str(i),'.wav'))
+    train_signal_normalized(i,:) = train_signal_preemph(i,:) ./ norm(...
+        train_signal_preemph(i,:),'Inf');
 end
+
+% % plot normalized signals
+% figure('Name','Normalized Training Data')
+% for i = 1:TRAIN_REC_CNT
+%     subplot(2,ceil(TRAIN_REC_CNT/2),i)
+%     plot(train_signal_normalized(i,:));
+%     title(strcat('s',num2str(i),'.wav'))
+% end
+% 
+% figure('Name','Normalized Training Data Spectrograms')
+% for i = 1:TRAIN_REC_CNT
+%     subplot(2,ceil(TRAIN_REC_CNT/2),i)
+%     spectrogram(train_signal_normalized(i,:),hamming(FRAME_SIZE_MS/1000*train_fs(i)),...
+%         FRAME_OVERLAP_MS/1000*train_fs(i),FFT_NUM_POINTS,train_fs(i));
+%     title(strcat('s',num2str(i),'.wav'))
+% end
+
+%% extract word envelopes from signals
+
+word_signal = zeros(TRAIN_REC_CNT,ceil(WORD_LENGTH_MS/1000*12500));
+for i = 1:TRAIN_REC_CNT
+    word_signal(i,:) = word_extract(train_signal_normalized(i,:),...
+        train_fs(i),WORD_DETECT_THRESH_DB,WORD_LENGTH_MS);
+end
+
+% % plot extracted words
+% figure('Name','Extracted Word Signals')
+% for i = 1:TRAIN_REC_CNT
+%     subplot(2,ceil(TRAIN_REC_CNT/2),i)
+%     plot(word_signal(i,:));
+%     title(strcat('s',num2str(i),'.wav'))
+% end
+% 
+% figure('Name','Extracted Word Signals Spectrograms')
+% for i = 1:TRAIN_REC_CNT
+%     subplot(2,ceil(TRAIN_REC_CNT/2),i)
+%     spectrogram(word_signal(i,:),hamming(FRAME_SIZE_MS/1000*train_fs(i)),...
+%         FRAME_OVERLAP_MS/1000*train_fs(i),FFT_NUM_POINTS,train_fs(i));
+%     title(strcat('s',num2str(i),'.wav'))
+% end
 
 %% Calculate the Mel-Frequency Cepstrum Coefficients
 
 % calculate MFCC coefficients
 mfcc_coeffs = cell(1,TRAIN_REC_CNT);
 for i = 1:TRAIN_REC_CNT
-    mfcc_coeffs{i} = mfcc(train_signal_preemph(i,:), train_fs(1), ... 
+    mfcc_coeffs{i} = mfcc(word_signal(i,:), train_fs(1), ... 
         FRAME_SIZE_MS,FRAME_OVERLAP_MS, FFT_NUM_POINTS, MEL_NUM_BANKS, ...
         CEPS_START_BANK, CEPS_NUM_BANKS);
 end
---
+++ matlab/word_extract.m
@@ -0,0 +1,76 @@
+function y = word_extract(x,Fs,THRESH_DB,WORD_LENGTH_MS,show_plots)
+%word_extract Extract word segment out of a signal
+%
+% USAGE: y = word_extract(x,fs,thresh_db,word_length_ms,show_plots)
+%
+% INPUTS:
+%   x - Input signal
+%   fs - Input signal sample rate
+%   thresh_db - Detection threshold in dB. Determines the power of the that
+%       the input signal needs to go over for the word to be detected.
+%   word_length_ms - The amount of time to extract for the word.
+%   show_plots - Generated plots for debugging.
+%
+% OUTPUTS:
+%   y - Output signal that contains the extracted word segment.
+
+% params
+STFT_WIN_LEN_MS    = 20;
+STFT_WIN_OVRLAP_MS = 15;
+STFT_FFT_LEN       = 1024;
+
+% normalize amplitude to [-0.5:0.5]
+x_norm = (1/2) * x ./ norm(x,'Inf');
+
+% generate window
+win_num_smpls = ceil(STFT_WIN_LEN_MS/1000*Fs);
+win_ovrlp_smpls = ceil(STFT_WIN_OVRLAP_MS/1000*Fs);
+w = hamming(win_num_smpls,'periodic');
+
+% STFT
+[s,s_freq,s_time] = spectrogram(x_norm,w,win_ovrlp_smpls,STFT_FFT_LEN,Fs);
+
+% PSD estimate of each frame; note that it is scaled such that peak power 
+% is 0 dB at peak signal amplitude
+s_psd = (abs(s).^2)/sum(w);
+
+% calculate avg power and peak power in each frame
+s_avg_pwr = var(s_psd);
+s_peak_pwr = max(s_psd);
+
+% determine which frames contain avg. power over the threshold
+ovr_thresh_idx = find(db(s_avg_pwr,'power') > THRESH_DB);
+
+% determine start and end of word envelope
+word_start_idx = floor(s_time(ovr_thresh_idx(1))*Fs);
+word_end_idx = word_start_idx + ceil(WORD_LENGTH_MS/1000*Fs)-1;
+
+y = x(word_start_idx:word_end_idx);
+
+if ~exist('show_plots','var')
+    show_plots = 0;
+end
+
+if show_plots
+    figure('Name','Spectrogram')
+    imagesc(s_freq,s_time,db(s_psd,'power')')
+    axis xy
+    colorbar
+
+    figure('Name','Word Extract Plots')
+    subplot(4,1,1)
+    plot(x)
+    title('Input Signal')
+    subplot(4,1,2)
+    plot(db(s_avg_pwr,'power'))
+    title('Input Sig. Avg. Power')
+    subplot(4,1,3)
+    plot(db(s_peak_pwr,'power'))
+    title('Input Sig. Peak Power')
+    subplot(4,1,4)
+    plot(y)
+    title('Output Signal')
+end 
+
+end
+
