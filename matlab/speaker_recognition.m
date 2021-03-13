9e66709f96fe5bf09ce41332c67557d227f66c1a
Igor Sheremet <isheremet@ucdavis.edu>
2021-03-13 15:12:45 -0800

    Cleaned up the code a bit and commented some plots out to speed it up.

--- matlab/speaker_recognition.m
+++ matlab/speaker_recognition.m
@@ -44,12 +44,6 @@ CODEBOOK_MFCC = [1 3 7 8 9 11];
 CODEBOOK_FIGS = [[1 9 7];
                  [11 3 1]];
 
-% Trim down the files for the "important part"
-% TODO: make the word envelope detection automatic
-TRIM_LEN          = 13000;
-TRAIN_TRIM_OFFSET = 2500;
-TEST_TRIM_OFFSET  = 2500;
-
 %% Read in a training file
 
 train_signal = zeros([TRAIN_REC_CNT,MAX_LEN]);
@@ -72,46 +66,34 @@ for i = 1:TRAIN_REC_CNT
     disp(strcat(file,' length: ',num2str(length(tmp_wav))))
 end
 
-% % Trim down training data
-% % TODO: make the word envelope detection automatic
-% train_signal(:,1:TRIM_LEN) = train_signal(:,TRAIN_TRIM_OFFSET:...
-%     (TRAIN_TRIM_OFFSET+TRIM_LEN-1));
-% train_signal(:,TRIM_LEN+1:end) = [];
-
-% plot spectrograms of training data to visualize
-figure('Name','Training Data Spectrograms')
-for i = 1:TRAIN_REC_CNT
-    subplot(2,ceil(TRAIN_REC_CNT/2),i)
-    spectrogram(train_signal(i,:),hamming(FRAME_SIZE_MS/1000*train_fs(i)),...
-        FRAME_OVERLAP_MS/1000*train_fs(i),FFT_NUM_POINTS,train_fs(i));
-    title(strcat('s',num2str(i),'.wav'))
-end
+% % plot spectrograms of training data to visualize
+% figure('Name','Training Data Spectrograms')
+% for i = 1:TRAIN_REC_CNT
+%     subplot(2,ceil(TRAIN_REC_CNT/2),i)
+%     spectrogram(train_signal(i,:),hamming(FRAME_SIZE_MS/1000*train_fs(i)),...
+%         FRAME_OVERLAP_MS/1000*train_fs(i),FFT_NUM_POINTS,train_fs(i));
+%     title(strcat('s',num2str(i),'.wav'))
+% end
 
 %% Read in a testing file
 
-% test_signal = zeros([TEST_REC_CNT,MAX_LEN]);
-% test_fs = zeros([TEST_REC_CNT,1]);
-% 
-% for i = 1:TEST_REC_CNT
-%     file = strcat('../data/Test_Data/s',num2str(i),'.wav');
-%     [tmp_wav, test_fs(i)] = audioread(file);
-%     
-%     % check if stereo
-%     if size(tmp_wav,2) > 1
-%         test_signal(i,1:length(tmp_wav)) = tmp_wav(:,CHANNEL);
-%     else
-%         test_signal(i,1:length(tmp_wav)) = tmp_wav;
-%     end
-%     
-%     disp(strcat(file,' length: ',num2str(length(tmp_wav))))
-% end
-% 
-% % Trim down test data
-% % TODO: make the word envelope detection automatic
-% test_signal(:,1:TRIM_LEN) = test_signal(:,TEST_TRIM_OFFSET:...
-%     (TEST_TRIM_OFFSET+TRIM_LEN-1));
-% test_signal(:,TRIM_LEN+1:end) = [];
-% 
+test_signal = zeros([TEST_REC_CNT,MAX_LEN]);
+test_fs = zeros([TEST_REC_CNT,1]);
+
+for i = 1:TEST_REC_CNT
+    file = strcat('../data/Test_Data/s',num2str(i),'.wav');
+    [tmp_wav, test_fs(i)] = audioread(file);
+    
+    % check if stereo
+    if size(tmp_wav,2) > 1
+        test_signal(i,1:length(tmp_wav)) = tmp_wav(:,CHANNEL);
+    else
+        test_signal(i,1:length(tmp_wav)) = tmp_wav;
+    end
+    
+    disp(strcat(file,' length: ',num2str(length(tmp_wav))))
+end
+
 % % plot spectrograms of test data to visualize
 % figure('Name','Test Data Spectrograms')
 % for i = 1:TEST_REC_CNT
@@ -169,21 +151,21 @@ for i = 1:TRAIN_REC_CNT
         train_fs(i),WORD_DETECT_THRESH_DB,WORD_LENGTH_MS);
 end
 
-% plot extracted words
-figure('Name','Extracted Word Signals')
-for i = 1:TRAIN_REC_CNT
-    subplot(2,ceil(TRAIN_REC_CNT/2),i)
-    plot(word_signal(i,:));
-    title(strcat('s',num2str(i),'.wav'))
-end
-
-figure('Name','Extracted Word Signals Spectrograms')
-for i = 1:TRAIN_REC_CNT
-    subplot(2,ceil(TRAIN_REC_CNT/2),i)
-    spectrogram(word_signal(i,:),hamming(FRAME_SIZE_MS/1000*train_fs(i)),...
-        FRAME_OVERLAP_MS/1000*train_fs(i),FFT_NUM_POINTS,train_fs(i));
-    title(strcat('s',num2str(i),'.wav'))
-end
+% % plot extracted words
+% figure('Name','Extracted Word Signals')
+% for i = 1:TRAIN_REC_CNT
+%     subplot(2,ceil(TRAIN_REC_CNT/2),i)
+%     plot(word_signal(i,:));
+%     title(strcat('s',num2str(i),'.wav'))
+% end
+% 
+% figure('Name','Extracted Word Signals Spectrograms')
+% for i = 1:TRAIN_REC_CNT
+%     subplot(2,ceil(TRAIN_REC_CNT/2),i)
+%     spectrogram(word_signal(i,:),hamming(FRAME_SIZE_MS/1000*train_fs(i)),...
+%         FRAME_OVERLAP_MS/1000*train_fs(i),FFT_NUM_POINTS,train_fs(i));
+%     title(strcat('s',num2str(i),'.wav'))
+% end
 
 %% Calculate the Mel-Frequency Cepstrum Coefficients
 
